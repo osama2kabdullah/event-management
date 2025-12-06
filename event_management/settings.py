e0698788ea77218d57a96def1a9bd85f3d8f39f7
Osama Abdullah <abdullah21673@hotmail.com>
2025-12-07 02:34:38 +0600

    static root file path get out of debug mode check

--- event_management/settings.py
+++ event_management/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
