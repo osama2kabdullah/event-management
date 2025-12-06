114a7046bb77bab407dcaf056873eb5815889366
Osama Abdullah <abdullah21673@hotmail.com>
2025-12-07 01:31:15 +0600

    allowed hosts provided

--- event_management/settings.py
+++ event_management/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-5nw%wdq1-%&52nagqzjbj%#a8u0-nuou&m-ep2%e5(p3p*ak8-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+  '*.onrender.com',
+]
 
 
 # Application definition
